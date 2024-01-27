8a87a07da621e0c17d538be6be3b581d3deea4a8
Dominik Moser <193428+hemeroc@users.noreply.github.com>
2024-01-27 13:09:26 +0100

    movement working

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "spieler":return img`
+. e e . . . . . . . . . . e e . 
+. e e e . . . . . . . . e e e . 
+. . e e e e e e e e e e e e . . 
+. . . e e e e e e e e e e . . . 
+. . . . e e e e e e e e . . . . 
+. . . . 3 . . . . . . 3 . . . . 
+. . . . 3 3 . 3 3 . 3 3 . . . . 
+. . . . . 3 . 3 3 . 3 . . . . . 
+. . . . . 3 9 9 9 9 3 . . . . . 
+. . . . . . 9 9 9 9 . . . . . . 
+. . . . . . 9 9 9 9 . . . . . . 
+. . . . . . 9 9 9 9 . . . . . . 
+. . . . . . 8 8 8 8 . . . . . . 
+. . . . . . 8 . . 8 . . . . . . 
+. . . . . . 8 . . 8 . . . . . . 
+. . . . . a a . . a a . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -1,1 +1,39 @@
-
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (spieler.left >= spieler.width) {
+        music.play(music.melodyPlayable(music.knock), music.PlaybackMode.InBackground)
+        spieler.left = spieler.left - spieler.width
+    } else {
+        music.play(music.melodyPlayable(music.buzzer), music.PlaybackMode.UntilDone)
+    }
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (spieler.right <= scene.screenWidth() - spieler.width) {
+        music.play(music.melodyPlayable(music.knock), music.PlaybackMode.InBackground)
+        spieler.right = spieler.right + spieler.width
+    } else {
+        music.play(music.melodyPlayable(music.buzzer), music.PlaybackMode.UntilDone)
+    }
+})
+function reduziereBudget (num: number) {
+    budget = budget - num
+    budgetAktualisieren()
+    if (budget < 0) {
+        game.gameOver(false)
+    }
+    if (budget <= 50) {
+        game.gameOver(true)
+    }
+}
+function budgetAktualisieren () {
+    budgetSprite = textsprite.create("" + convertToText(budget) + " Eur")
+    budgetSprite.top = 5
+    budgetSprite.right = scene.screenWidth() - 5
+}
+let budgetSprite: TextSprite = null
+let budget = 0
+let spieler: Sprite = null
+spieler = sprites.create(assets.image`spieler`, SpriteKind.Player)
+spieler.left = spieler.width * 4
+spieler.bottom = scene.screenHeight()
+budget = 2640
+budgetAktualisieren()
